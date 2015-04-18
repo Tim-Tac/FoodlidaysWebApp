71a58958f39b36c8b8c96e6a6abc36ae40c7b304
Timothy Deprince <titidep001@hotmail.com>
2015-04-19 01:29:38 +0200

    maj wdk + part of connection

--- www/js/app.js
+++ www/js/app.js
@@ -6,8 +6,33 @@ function ManageForm()
     {
         alert("nok");
     }
-    else 
+    else alert("ok");
+}
+
+
+function post(room_number, params, method) 
+{
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("room_number", method);
+    form.setAttribute("email", path);
+
+    for(var key in params) 
     {
-        alert("ok");
+        if(params.hasOwnProperty(key)) 
+        {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
     }
+
+    document.body.appendChild(form);
+    form.submit();
 }
