db4bec0542fbde5df20bcbe2136fe1db0f57aa7d
Tim Dep <titidep001@hotmail.com>
2015-04-18 19:02:13 +0200

    maj

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,13 @@
-/*
- * Please see the included README.md file for license terms and conditions.
- */
+// This file contains event handlers, the center of your application.
 
-
-/*jslint browser:true, devel:true, white:true, vars:true */
-/*global $:false, intel:false app:false, dev:false, cordova:false */
-
-
-
-// This file contains your event handlers, the center of your application.
-// NOTE: see app.initEvents() in init-app.js for event handler initialization code.
-
-// function myEventHandler() {
-//     "use strict" ;
-// // ...event handler code here...
-// }
-
-
-// ...additional event handlers here...
+function ManageForm()
+{
+    if((document.getElementById("input_email").value == "") || (document.getElementById("input_room").value == ""))
+    {
+        alert("nok");
+    }
+    else 
+    {
+        alert("ok");
+    }
+}
