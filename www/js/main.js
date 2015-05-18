cd8612d4210f2c8fb7fc1d4c46a7b82b3436f754
Tim Dep <titidep001@hotmail.com>
2015-05-18 19:41:20 +0200

    try of event click head

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function() 
 {
+    /************MENU CLICK****************/
     $("#menu").click(function(event)
     {
-        alert("menu"):
+        alert("menu");
     });
+    
+    /************PANIER CLICK****************/
+    $("#panier").click(function(event)
+    {
+        alert("panier");
+        $("#container").css('background-image', 'url(../images/home_slide1.png)');
+        $("#container").html("hello");
+    });
+    
+    /************PROFIL CLICK****************/
+    $("#profil").click(function(event)
+    {
+        alert("profil");
+        $("#container").css('background-color', 'red');
+    });
+    
 });
