d277eaa4b6f44a71656627378d3791fbdca88ca1
Tim Dep <titidep001@hotmail.com>
2015-05-20 20:49:04 +0200

    better list + no more loading on ready

--- www/js/main.js
+++ www/js/main.js
@@ -1,32 +1,23 @@
 $(document).ready(function() 
 {
-    //when launched, we load menu by default
-    var just_launched = 1;
-    $.get( "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/" + localStorage.zip, 
-    function( data ) 
-    {
-        for(var i = 0 ; i<data.length ; i++)
-        {
-            $( "#container" ).append("<table> <tr> <td>" +  data[i].name + "</td> <td>" + data[i].price + "</td> </tr> </table>");
-        }
-    },"json").fail(function()
-    {
-        alert( "A network error occured, please check your internet connexion or try again later" );
-    });
+
     
     
     
-    /************MENU CLICK****************/
+    
+    $("#menu").click();
+    
+    /*************************************MENU CLICK**************************************/
     $("#menu").click(function(event)
-    {
-        if(just_launched == 1) return;
+    {   
         $("#container").html("");
         $.get( "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/" + localStorage.zip, 
         function( data ) 
         {
+            
             for(var i = 0 ; i<data.length ; i++)
             {
-                $( "#container" ).append("<table> <tr> <td>" +  data[i].name + "</td> <td>" + data[i].price + "</td> <td> <button> + </button> </td> </tr> </table> ");
+                $( "#container" ).append("<tr> <td>  <img class=\"image_food\" src=\"http://foodlidays.dev.innervisiongroup.com/uploads/" +data[i].image + "\"></img> </td> <td>" + data[i].name + "</td> <td> " + data[i].note + " </td> <td> price </td> </tr>");
             }
         },"json").fail(function() 
         {
@@ -36,22 +27,19 @@ $(document).ready(function()
     
     
     
-    /************PANIER CLICK****************/
+    /**********************************PANIER CLICK****************************************/
     $("#panier").click(function(event)
     {
-        just_launched = 0;
         $("#container").html("panier");
     });
     
     
     
-    /************PROFIL CLICK****************/
+    /***********************************PROFIL CLICK***************************************/
     $("#profil").click(function(event)
     {
-        just_launched = 0;
         $("#container").html("profil");
     });
     
-    /**********CLICK PRODUCT + *************/
     
 });
