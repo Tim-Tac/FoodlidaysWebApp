b17c3c461b94e47da3df22608d91bdddb86bf392
Tim Dep <titidep001@hotmail.com>
2015-05-20 17:15:29 +0200

    load food when click on menu + fix html content re-print

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() 
-{ 
+{
+    //when launched, we load menu by default
     var just_launched = 1;
     $.get( "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/" + localStorage.zip, 
     function( data ) 
@@ -8,18 +9,32 @@ $(document).ready(function()
         {
             $( "#container" ).append("<table> <tr> <td>" +  data[i].name + "</td> <td>" + data[i].price + "</td> </tr> </table>");
         }
-    },"json").fail(function() 
+    },"json").fail(function()
     {
         alert( "A network error occured, please check your internet connexion or try again later" );
     });
+    
     
     
     /************MENU CLICK****************/
     $("#menu").click(function(event)
     {
         if(just_launched == 1) return;
-        $("#container").html("<h1> test </h1>");
+        $("#container").html("");
+        $.get( "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/" + localStorage.zip, 
+        function( data ) 
+        {
+            for(var i = 0 ; i<data.length ; i++)
+            {
+                $( "#container" ).append("<table> <tr> <td>" +  data[i].name + "</td> <td>" + data[i].price + "</td> </tr> </table>");
+            }
+        },"json").fail(function() 
+        {
+            alert( "A network error occured, please check your internet connexion or try again later" );
+        });
     });
+    
+    
     
     /************PANIER CLICK****************/
     $("#panier").click(function(event)
@@ -27,6 +42,8 @@ $(document).ready(function()
         just_launched = 0;
         $("#container").html("panier");
     });
+    
+    
     
     /************PROFIL CLICK****************/
     $("#profil").click(function(event)
