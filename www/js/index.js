57df00709bb865388a8505b502a17570f4ff4077
Tim Dep <titidep001@hotmail.com>
2015-05-21 21:59:06 +0200

    change and management structure

--- www/js/index.js
+++ www/js/index.js
@@ -1,115 +1,68 @@
-function checkNetConnection()
-{
-     jQuery.ajaxSetup({async:false});
-     re="";
-     r=Math.round(Math.random() * 10000);
-     $.get("http://demos.subinsb.com/cdn/dot.png",{subins:r},function(d){
-      re=true;
-     }).error(function(){
-      re=false;
-     });
-     return re;
-}
-
-
-$(document).ready(function()
+$(document).ready(function() 
 {
     
-    $("#qr").click(function(event)
-    {
-       document.addEventListener("intel.xdk.device.barcode.scan",function(evt)
-        {
-            if (evt.success == true) 
-            {
-                if (evt.codedata == "http://www.sampleurl.com/fake.html")
-                {
-                        //nothing to do
-                }
-                else
-                {
-                    document.getElementById('input_room').value = evt.codedata;
-                }
-            }else 
-            {
-                //failed scan
-                console.log("failed scan");
-            }
-       },false);
-        intel.xdk.device.scanBarcode();
+    //define Article object
+    var Article = {
+        name : "",
+        quantity : ""
+    };
+    var articles = [];
+    
+    $("#menu").click();
+    
+    /*************************************MENU CLICK**************************************/
+    $("#menu").click(function(event)
+    {  
+        $("#container").html("");
+        $("body").css("background-image", "url(../images/home_slide1.png)");
         
+        $.get( "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/" + localStorage.zip, 
+        function( data ) 
+        {
+            
+            for(var i = 0 ; i<data.length ; i++)
+            {
+                $( "#container" ).append("<tr> <td>  <img class=\"image_food\" src=\"http://foodlidays.dev.innervisiongroup.com/uploads/" +data[i].image + "\"></img> </td> <td> <table> <tr> <td>" + data[i].name + "</td> </tr> <tr> <td> " + data[i].note + " </td> </tr> </table> </td> <td> "  + data[i].price  + " </td> </tr>");
+                
+            }
+        },"json").fail(function() 
+        {
+            alert( "A network error occured, please check your internet connexion or try again later" );
+        });
     });
     
     
     
-    /*POST request to try to connect the user*/
-    $("#valid").click(function(event)
+    /**********************************PANIER CLICK****************************************/
+    $("#panier").click(function(event)
     {
-        var formOK = (document.getElementById('input_email').value !== "") && (document.getElementById('input_room').value !== "");
-        if(formOK)
+        $("#container").html("");
+        $("body").css("background-image", "url(../images/home_slide1.png)");
+        
+        if(articles.length === 0) $("#container").html("<h3> Votre panier est vide </h3>");
+        else 
         {
-            if(checkNetConnection)
-            {
-                    $.post(
-                        "http://foodlidays.dev.innervisiongroup.com/api/v1/login",
-                        {  email : document.getElementById("input_email").value, //a remplacer par les valeurs des champs
-                            room_number : document.getElementById("input_room").value },
-                        function(data, status)
-                        {
-                            localStorage.connected = "true";
-                            localStorage.user_email = data.email;
-                            localStorage.place_type = data.place_type;
-                            localStorage.id = data.room.id;
-                            localStorage.user_id = data.room.user_id;
-                            localStorage.street_address = data.room.street_address;
-                            localStorage.city = data.room.city;
-                            localStorage.country = data.room.country;
-                            localStorage.zip = data.room.zip;
-                            localStorage.room_number = data.room.room_number;
-                            
-                            if(data.place_type == "place")
-                            {
-                                localStorage.name_place = data.room.name;
-                            }
-                            else if(data.place_type == "room")
-                            {
-                                localStorage.floor = data.room.floor;
-                                localStorage.room = data.room.room;
-                            }
-                            
-                            window.open("main.html");
-                        },
-                        "json"
-                        ).fail(function() 
-                        {
-                            alert( "A network error occured, please check your internet connexion or try again later" );
-                        });
-            }
-            else alert("Internet connexion is required");
+            $("#container").html("liste panier + bouton achat");
         }
-        else alert("Both fields are required");
-    });   
-});
-
-
-
-
-
-/** Try for disable back button **/
-
-$(window).on("navigate", function (event, data) {
-  var direction = data.state.direction;
-  if (direction == 'back') {
-    alert("back");
-  }
-  if (direction == 'forward') {
-    // do something else
-  }
-});
-
-
-$(document).live("pagebeforechange", function(e,ob) {
-    if(ob.toPage && (typeof ob.toPage==="string") && ob.toPage.indexOf('index.html') >= 0) {
-        console.log("blocking the back");
-        e.preventDefault();
-    }
+    });
+    
+    
+    
+    /***********************************PROFIL CLICK***************************************/
+    $("#profil").click(function(event)
+    {
+        $("#container").html("");
+        $("body").css("background-image", "url(../images/home_slide3.png)");
+        
+        $("#container").append("Identifiant de chambre : " + localStorage.room_number + "</br> Votre email : " + localStorage.user_email + "</br> </br> Localisation de la chambre : </br> " + localStorage.street_address + " </br> " + localStorage.zip + " " + localStorage.city + "</br>" + localStorage.floor + "e étage, chambre " + localStorage.room);
+        
+    });
+    
+    
+    $("#container").click(function(event)
+    {
+        alert("ok");
+    });
+    
+    
 });
---
+++ www/js/login.js
@@ -0,0 +1,114 @@
+function checkNetConnection()
+{
+     jQuery.ajaxSetup({async:false});
+     re="";
+     r=Math.round(Math.random() * 10000);
+     $.get("http://demos.subinsb.com/cdn/dot.png",{subins:r},function(d){
+      re=true;
+     }).error(function(){
+      re=false;
+     });
+     return re;
+}
+
+
+$(document).ready(function()
+{   
+    $("#qr").click(function(event)
+    {
+       document.addEventListener("intel.xdk.device.barcode.scan",function(evt)
+        {
+            if (evt.success == true) 
+            {
+                if (evt.codedata == "http://www.sampleurl.com/fake.html")
+                {
+                        //nothing to do
+                }
+                else
+                {
+                    document.getElementById('input_room').value = evt.codedata;
+                }
+            }else 
+            {
+                //failed scan
+                console.log("failed scan");
+            }
+       },false);
+        intel.xdk.device.scanBarcode();
+        
+    });
+    
+    
+    
+    /*POST request to try to connect the user*/
+    $("#valid").click(function(event)
+    {
+        var formOK = (document.getElementById('input_email').value !== "") && (document.getElementById('input_room').value !== "");
+        if(formOK)
+        {
+            if(checkNetConnection)
+            {
+                    $.post(
+                        "http://foodlidays.dev.innervisiongroup.com/api/v1/login",
+                        {  email : document.getElementById("input_email").value, //a remplacer par les valeurs des champs
+                            room_number : document.getElementById("input_room").value },
+                        function(data, status)
+                        {
+                            localStorage.connected = "true";
+                            localStorage.user_email = data.email;
+                            localStorage.place_type = data.place_type;
+                            localStorage.id = data.room.id;
+                            localStorage.user_id = data.room.user_id;
+                            localStorage.street_address = data.room.street_address;
+                            localStorage.city = data.room.city;
+                            localStorage.country = data.room.country;
+                            localStorage.zip = data.room.zip;
+                            localStorage.room_number = data.room.room_number;
+                            
+                            if(data.place_type == "place")
+                            {
+                                localStorage.name_place = data.room.name;
+                            }
+                            else if(data.place_type == "room")
+                            {
+                                localStorage.floor = data.room.floor;
+                                localStorage.room = data.room.room;
+                            }
+                            
+                            window.open("main.html");
+                        },
+                        "json"
+                        ).fail(function() 
+                        {
+                            alert( "A network error occured, please check your internet connexion or try again later" );
+                        });
+            }
+            else alert("Internet connexion is required");
+        }
+        else alert("Both fields are required");
+    });   
+});
+
+
+
+
+
+/** Try for disable back button **/
+
+$(window).on("navigate", function (event, data) {
+  var direction = data.state.direction;
+  if (direction == 'back') {
+    alert("back");
+  }
+  if (direction == 'forward') {
+    // do something else
+  }
+});
+
+
+$(document).live("pagebeforechange", function(e,ob) {
+    if(ob.toPage && (typeof ob.toPage==="string") && ob.toPage.indexOf('index.html') >= 0) {
+        console.log("blocking the back");
+        e.preventDefault();
+    }
+});
