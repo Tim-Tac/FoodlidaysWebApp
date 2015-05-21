cc212224961eaa13a60128d2fdb7e9ec92c441c9
Tim Dep <titidep001@hotmail.com>
2015-05-21 21:40:16 +0200

    container listener + profil info

--- www/js/main.js
+++ www/js/main.js
@@ -7,21 +7,21 @@ $(document).ready(function()
     };
     var articles = [];
     
-
     $("#menu").click();
-    
     
     /*************************************MENU CLICK**************************************/
     $("#menu").click(function(event)
-    {   
+    {  
         $("#container").html("");
+        $("body").css("background-image", "url(../images/home_slide1.png)");
+        
         $.get( "http://foodlidays.dev.innervisiongroup.com/api/v1/food/cat/all/" + localStorage.zip, 
         function( data ) 
         {
             
             for(var i = 0 ; i<data.length ; i++)
             {
-                $( "#container" ).append("<tr> <td>  <img class=\"image_food\" src=\"http://foodlidays.dev.innervisiongroup.com/uploads/" +data[i].image + "\"></img> </td> <td> <table> <tr> <td>" + data[i].name + "</td> </tr> <tr> <td> " + data[i].note + " </td> </tr> </table> </td> <td> price </td> </tr>");
+                $( "#container" ).append("<tr> <td>  <img class=\"image_food\" src=\"http://foodlidays.dev.innervisiongroup.com/uploads/" +data[i].image + "\"></img> </td> <td> <table> <tr> <td>" + data[i].name + "</td> </tr> <tr> <td> " + data[i].note + " </td> </tr> </table> </td> <td> "  + data[i].price  + " </td> </tr>");
                 
             }
         },"json").fail(function() 
@@ -35,11 +35,13 @@ $(document).ready(function()
     /**********************************PANIER CLICK****************************************/
     $("#panier").click(function(event)
     {
-        if(articles.length === 0) $("#container").html("panier");
+        $("#container").html("");
+        $("body").css("background-image", "url(../images/home_slide1.png)");
+        
+        if(articles.length === 0) $("#container").html("<h3> Votre panier est vide </h3>");
         else 
         {
-            alert(articles.length);
-            $("#container").html("<h3> Votre panier est vide </h3>");
+            $("#container").html("liste panier + bouton achat");
         }
     });
     
@@ -48,9 +50,17 @@ $(document).ready(function()
     /***********************************PROFIL CLICK***************************************/
     $("#profil").click(function(event)
     {
-        $("#container").html("profil");
-        $("#container").css("background-image", "url(../images/home_slide3.png)");
-
+        $("#container").html("");
+        $("body").css("background-image", "url(../images/home_slide3.png)");
+        
+        $("#container").append("Identifiant de chambre : " + localStorage.room_number + "</br> Votre email : " + localStorage.user_email + "</br> </br> Localisation de la chambre : </br> " + localStorage.street_address + " </br> " + localStorage.zip + " " + localStorage.city + "</br>" + localStorage.floor + "e étage, chambre " + localStorage.room);
+        
+    });
+    
+    
+    $("#container").click(function(event)
+    {
+        alert("ok");
     });
     
     
